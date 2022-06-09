e393054f666bbf91cd56187333bd8eeeb5b06c3b
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-09 15:42:27 -0300

    mudança de tipagem para que não seja possível alterar o valor original pelo controller

--- dist/js/models/Negociacoes.js
+++ dist/js/models/Negociacoes.js
@@ -5,6 +5,7 @@ export class Negociacoes {
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
+    //o lista retorna um array, se eu deixar apenas a tipagem de Array<Negociacao>, pelo meu controller ainda fica possível manipular esse array que é retornado. Para que isso não aconteça, posso retornar uma cópia do array, passando o spread operator, para que o valor não aponte para o endereço de memória do array original, e assim, não seja modificado, ou, posso passar ReadonlyArray como tipagem, assim, dentro de controller, nem me é sugerido qualquer método que faça a manipulação do valor original, por exemplo um pop, apenas métodos que não modifiquem o array original, pois passo ele somente como leitura.
     lista() {
         return this._negociacoes;
     }
