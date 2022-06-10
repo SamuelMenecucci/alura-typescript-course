8d3ef55a9532515ba09e76fc04027efc893dd071
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-10 17:36:25 -0300

    anotações e hardcoded enum values

--- app/enums/dias-da-semana.ts
+++ app/enums/dias-da-semana.ts
@@ -1,11 +1,15 @@
 //DOC https://www.typescriptlang.org/docs/handbook/enums.html
 
+//com enum eu consigo trabalhar com constantes e utilizar em toda a minha aplicação.
+//se eu não passar nenhum valor como referencia para essa constante, o typescript coloca em sequencia numérica, iniciando de 0
+//se eu iniciar somente a primeira constante com algum valor numérico, ele irá colocar automaticamente a sequencia numérica para os próximos, caso não seja dada.
+//se no meio eu adicionar somente um valor, após aquela constante, a lógica irá seguir a partir daquele valor.
 export enum DiasDaSemana {
-  DOMINGO,
-  SEGUNDA,
-  TERCA,
-  QUARTA,
-  QUINTA,
-  SEXTA,
-  SABADO,
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERCA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6,
 }
--- dist/js/enums/dias-da-semana.js
+++ dist/js/enums/dias-da-semana.js
@@ -1,4 +1,8 @@
 //DOC https://www.typescriptlang.org/docs/handbook/enums.html
+//com enum eu consigo trabalhar com constantes e utilizar em toda a minha aplicação.
+//se eu não passar nenhum valor como referencia para essa constante, o typescript coloca em sequencia numérica, iniciando de 0
+//se eu iniciar somente a primeira constante com algum valor numérico, ele irá colocar automaticamente a sequencia numérica para os próximos, caso não seja dada.
+//se no meio eu adicionar somente um valor, após aquela constante, a lógica irá seguir a partir daquele valor.
 export var DiasDaSemana;
 (function (DiasDaSemana) {
     DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
