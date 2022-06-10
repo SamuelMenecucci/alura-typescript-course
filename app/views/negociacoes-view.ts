ab9caba2e7312fcf626e9649e86bf314c8fa43a9
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-10 10:19:14 -0300

    criando um template de uma tabela para ver os dados de negociacoes

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,23 @@
+export class NegociacoesView {
+  template(): string {
+    return `
+    <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    `;
+  }
+}
