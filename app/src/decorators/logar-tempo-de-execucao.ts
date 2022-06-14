c4b568fc6ccbb47025344e215b2dd737c53cb896
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-14 12:06:48 -0300

    passando parâmetros para o decorator

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
   return function (
     //o target pode ser duas coisas: Se o decorator for colocado em um método estático esse target é a função construtora da classe. se for colocado em um método que não é estático ele vai retornar o prototype da classe
     target: any,
@@ -11,13 +11,21 @@ export function logarTempoDeExecucao() {
   ) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args: Array<any>) {
+      let divisor = 1;
+      let unidade = "milissegundos";
+
+      if (emSegundos) {
+        divisor = 1000;
+        unidade = "segundos";
+      }
+
       const t1 = performance.now();
       const retorno = metodoOriginal.apply(this, args);
       const t2 = performance.now();
       console.log(
-        `${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`
       );
-      return retorno;
+      retorno;
     };
 
     //uma função decorator tem que retornar um descriptor
